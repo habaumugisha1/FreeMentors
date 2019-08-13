d6034927b23f085458186a7f83c104d1faf1991e
EugeneMunya <eugenemu57@gmail.com>
2019-08-13 11:20:59 +0200

    feature(dashboard):create admin dashbord[Finishes #167617649]

---
+++ UI/script/script.js
@@ -0,0 +1,18 @@
+const tabLinks = document.querySelectorAll(".tabs a");
+const pannels = document.querySelectorAll(".pannel");
+
+tabLinks.forEach(link =>{
+link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(".tabs li.active").classList.remove("active");
+    document.querySelector(".pannel.active").classList.remove("active");
+    const parentEl = link.parentElement;
+    parentEl.classList.add("active");
+
+    const index = [...parentEl.parentElement.children].indexOf(parentEl);
+    const pannel = [...pannels].filter(el =>el.getAttribute('data-index') == index);
+    console.log(pannel)
+    pannel[0].classList.add("active");
+
+})
+});
