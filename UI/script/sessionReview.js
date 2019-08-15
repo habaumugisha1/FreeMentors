62e8e3ddd8568b1c1a9f6e3cd223a22135a6fa45
EugeneMunya <eugenemu57@gmail.com>
2019-08-15 13:26:14 +0200

    feature(mentors):view all mentors[Finishes #167617769]

--- UI/script/sessionReview.js
+++ UI/script/sessionReview.js
@@ -1,22 +1,40 @@
-const modalBg = document.querySelector('.modal-bg');
-const closeModal = document.querySelector('.modal-close');
+const modalBg = document.querySelectorAll('.modal-bg');
+const closeModal = document.querySelectorAll('.modal-close');
 const sessionReview = document.querySelectorAll('.session-review');
-const deleteBtn = document.querySelector('.delete-btn');
+const deleteBtn = document.querySelectorAll('.delete-btn');
+const updateProfile = document.querySelector('.update');
+const updateProfileBtModal = document.querySelector('.update-profile-btn');
 
 
 sessionReview.forEach(session =>{
     session.addEventListener('click', e =>{
-        modalBg.style.visibility= 'visible';
-        modalBg.style.opacity = 1; 
+        modalBg[0].style.visibility= 'visible';
+        modalBg[0].style.opacity = 1; 
     })
 })
 
-closeModal.addEventListener('click',e => {
-    modalBg.style.visibility= 'hidden';
-    modalBg.style.opacity = 0;
+closeModal[0].addEventListener('click',e => {
+    modalBg[0].style.visibility= 'hidden';
+    modalBg[0].style.opacity = 0;
     })
     
-deleteBtn.addEventListener('click',e => {
-    modalBg.style.visibility= 'hidden';
-    modalBg.style.opacity = 0;
+deleteBtn.forEach(btn =>{
+    btn.addEventListener('click', e =>{
+        modalBg[0].style.visibility= 'hidden';
+        modalBg[0].style.opacity = 0; 
     })
+})
+
+    updateProfile.addEventListener('click', e => {
+        modalBg[1].style.visibility= 'visible';
+         modalBg[1].style.opacity = 1; 
+    })
+
+    updateProfileBtModal.addEventListener('click',e => {
+        modalBg[1].style.visibility= 'hidden';
+        modalBg[1].style.opacity = 0;
+        });
+ closeModal[1].addEventListener('click',e => {
+            modalBg[1].style.visibility= 'hidden';
+            modalBg[1].style.opacity = 0;
+            })
