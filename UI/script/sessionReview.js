3acd5ffdae8e406d236361ce657870edc9b52be8
EugeneMunya <eugenemu57@gmail.com>
2019-08-16 14:27:05 +0200

    chore(responsiveness):dispaly all pages in mobile view port[Finishes #167923724]

--- UI/script/sessionReview.js
+++ UI/script/sessionReview.js
@@ -8,33 +8,39 @@ const updateProfileBtModal = document.querySelector('.update-profile-btn');
 
 sessionReview.forEach(session =>{
     session.addEventListener('click', e =>{
+        e.preventDefault();
         modalBg[0].style.visibility= 'visible';
         modalBg[0].style.opacity = 1; 
     })
 })
 
 closeModal[0].addEventListener('click',e => {
+    e.preventDefault();
     modalBg[0].style.visibility= 'hidden';
     modalBg[0].style.opacity = 0;
     })
     
 deleteBtn.forEach(btn =>{
     btn.addEventListener('click', e =>{
+        e.preventDefault();
         modalBg[0].style.visibility= 'hidden';
         modalBg[0].style.opacity = 0; 
     })
 })
 
     updateProfile.addEventListener('click', e => {
+        e.preventDefault();
         modalBg[1].style.visibility= 'visible';
          modalBg[1].style.opacity = 1; 
     })
 
     updateProfileBtModal.addEventListener('click',e => {
+        e.preventDefault();
         modalBg[1].style.visibility= 'hidden';
         modalBg[1].style.opacity = 0;
         });
  closeModal[1].addEventListener('click',e => {
+    e.preventDefault();
             modalBg[1].style.visibility= 'hidden';
             modalBg[1].style.opacity = 0;
             })
--- UI/script/userReviewSession.js
+++ UI/script/userReviewSession.js
@@ -6,53 +6,62 @@ const updateProfile = document.querySelector('.update');
 const updateSession = document.querySelectorAll('.edit-session');
 
 const updateSessionBtnModal = document.querySelector('.update-session-btn');
-const updateProfileBtModal = document.querySelector('.update-profile-btn');
+const updateProfileBtnModal = document.querySelector('.update-profile-btn');
  
 
 closeModal[0].addEventListener('click',e => {
+    e.preventDefault();
     modalBg[0].style.visibility= 'hidden';
     modalBg[[0]].style.opacity = 0;
     })
     
     reviewSession.forEach(session => {
         session.addEventListener('click', e => {
+            e.preventDefault();
             modalBg[0].style.visibility= 'visible';
             modalBg[0].style.opacity = 1; 
         })
     })
 
     saveReview.addEventListener('click', e => {
+        e.preventDefault();
         modalBg[0].style.visibility= 'hidden';
         modalBg[0].style.opacity = 0; 
     })
     
     updateProfile.addEventListener('click', e => {
+        e.preventDefault();
         modalBg[1].style.visibility= 'visible';
          modalBg[1].style.opacity = 1; 
     })
     closeModal[1].addEventListener('click',e => {
+        e.preventDefault();
         modalBg[1].style.visibility= 'hidden';
         modalBg[1].style.opacity = 0;
         })
 
     updateSession.forEach(session =>{
         session.addEventListener('click', e=>{
+            e.preventDefault();
             modalBg[2].style.visibility= 'visible';
             modalBg[2].style.opacity = 1;  
         })
     })
 
     closeModal[2].addEventListener('click',e => {
+        e.preventDefault();
         modalBg[2].style.visibility= 'hidden';
         modalBg[2].style.opacity = 0;
         })
 
-        updateProfileBtModal.addEventListener('click',e => {
+        updateProfileBtnModal.addEventListener('click',e => {
+            e.preventDefault();
             modalBg[1].style.visibility= 'hidden';
             modalBg[1].style.opacity = 0;
             });
 
       updateSessionBtnModal.addEventListener('click',e => {
+        e.preventDefault();
         modalBg[2].style.visibility= 'hidden';
         modalBg[2].style.opacity = 0;
         });
