1648865d9826fb9849669865670972fe239b76c0
EugeneMunya <eugenemu57@gmail.com>
2019-08-21 16:57:01 +0200

    feature(reject-session):reject mentorship session endpoint[Finishes #167996726]

--- server/routes/sessionRoute/sessionRoute.js
+++ server/routes/sessionRoute/sessionRoute.js
@@ -7,4 +7,6 @@ const sessionRoute = express.Router({ mergeParams: true });
 sessionRoute.post('/sessions', routeMiddleware.canViewAllMentors, sessionRequest.createSessionRequest);
 sessionRoute.get('/sessions', routeMiddleware.canViewAllMentors, sessionRequestHandler.viewAllSessions);
 sessionRoute.patch('/sessions/:sessionId/accept', routeMiddleware.isMentor, sessionRequestHandler.acceptSession);
+sessionRoute.patch('/sessions/:sessionId/reject', routeMiddleware.isMentor, sessionRequestHandler.rejectSession);
+
 export default sessionRoute;
