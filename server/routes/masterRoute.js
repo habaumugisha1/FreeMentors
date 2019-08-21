5751ffa1a218a00ab9023c2a2647dd80eed76b91
EugeneMunya <eugenemu57@gmail.com>
2019-08-21 13:28:09 +0200

    feature(create):create mentorship session request endpoint[Finishes #167996306]

--- server/routes/masterRoute.js
+++ server/routes/masterRoute.js
@@ -1,6 +1,8 @@
 
-import userEndPoint from './userRoute/userEndPoint';
+import userEndPoint from './userRoute/userRoute';
+import sessionEndPoint from './sessionRoute/sessionRoute';
 
 export default (app) => {
   app.use('/api/v1', userEndPoint);
+  app.use('/api/v1', sessionEndPoint);
 };
