1cdb1f721d5597e0c4118a9b2a4ebc2d48d707c6
EugeneMunya <eugenemu57@gmail.com>
2019-08-25 14:19:44 +0200

    chore(api-documentation):create swagger json to document api[ Finishes #168094027]

--- server/app.js
+++ server/app.js
@@ -2,17 +2,13 @@ import express from 'express';
 import { json, urlencoded } from 'body-parser';
 import cors from 'cors';
 import allowMethod from 'allow-methods';
-import swaggerUi from 'swagger-ui-express';
 import masterRoute from './routes/masterRoute';
-import swaggerDoc from '../swagger.json';
-
 
 const app = express();
 app.use(cors());
 app.use(json());
 app.use(allowMethod(['get', 'post', 'head', 'delete', 'patch'], 'Method Not allowed'));
 app.use(urlencoded({ extended: true }));
-app.get('/', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 masterRoute(app);
 app.listen(3000);
 export default app;
