4e4d95ce9079b2223bbb74c43652f2caecbc167f
EugeneMunya <eugenemu57@gmail.com>
2019-09-09 18:11:46 +0200

    feature(sign-up-in-database):store sign-up data in database [ Finishes #168370261]

--- server/models/database/dbConfig.js
+++ server/models/database/dbConfig.js
@@ -1,6 +1,8 @@
 export default {
+  host: 'localhost',
   user: 'postgres',
+  database: 'freementors',
+  password: '',
   port: 5432,
-  max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000,
+
 };
