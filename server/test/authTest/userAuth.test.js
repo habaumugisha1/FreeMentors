3aecfd31181b6bdc73fa0d6350c1c63d56db6fbe
EugeneMunya <eugenemu57@gmail.com>
2019-09-11 13:15:58 +0200

    chore(use-database):allow user to work with data from database [ Finishes #168425228]

--- server/test/authTest/userAuth.test.js
+++ server/test/authTest/userAuth.test.js
@@ -1,7 +1,7 @@
 import { expect, use, request } from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../../server';
-import { signUp, signIn } from '../testDummyData/mockData';
+import { signUp, signIn, userRole } from '../testDummyData/mockData';
 import dbClient from '../../models/database/dbClient';
 import { Users } from '../../models/database/dbTables';
 
@@ -14,6 +14,7 @@ describe('Auth test', () => {
     }).catch((error) => console.log(error)));
     done();
   });
+
   it('User signup', (done) => {
     request(server)
       .post('/api/v1/auth/signup')
@@ -27,7 +28,44 @@ describe('Auth test', () => {
     request(server)
       .post('/api/v1/auth/signin')
       .send(signIn).end((err, res) => {
-        expect(res).at.have.status(200);
+        expect(res).to.have.status(201);
+      });
+    done();
+  });
+  it('admin change mentee to mentor', (done) => {
+    request(server).patch('/api/v1/user/1')
+      .send(userRole)
+      .end((err, res) => {
+        expect(res).to.have.status(201);
+      });
+    done();
+  });
+  it('view all mentors', (done) => {
+    request(server).get('/api/v1/mentors')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('data');
+        expect(res.body.data).to.be.an('array');
+      });
+
+    done();
+  });
+  it('view specific mentor', (done) => {
+    request(server)
+      .get('/api/v1/mentors/1')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('data');
+      });
+    done();
+  });
+  it('View all users', (done) => {
+    request(server)
+      .get('/api/v1/users')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('data');
+        expect(res.body.data).to.be.an('array');
       });
     done();
   });
