cea408ec682cb81e54c82bb42a239427c36a64dd
EugeneMunya <eugenemu57@gmail.com>
2019-09-12 14:44:40 +0200

    chore(remote-database):put all request mentorship sessions information into the database [ Finshes #168425228]

--- server/models/database/dbConfig.js
+++ server/models/database/dbConfig.js
@@ -1,7 +1,10 @@
 export default {
-  host: 'localhost',
-  user: 'postgres',
-  database: 'freementors',
-  password: '',
+  host: 'ec2-107-22-238-217.compute-1.amazonaws.com',
+  user: 'slgwzksqbpgwpf',
+  database: 'd8q2lbmes6bdmu',
+  password: 'af8c9f7c7fba21911a68ef8741adb4690d82f6f0f88992ffb9b067ebe4442f8c',
   port: 5432,
+  ssl: true,
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000,
 };
