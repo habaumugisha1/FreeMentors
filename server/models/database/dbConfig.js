5e69897275b0e57e34fe75f7b01b48905515b7b0
EugeneMunya <eugenemu57@gmail.com>
2019-09-12 15:11:12 +0200

    chore(remote-database):put all request mentorship sessions information into the database [ Finshes #168425228]

--- server/models/database/dbConfig.js
+++ server/models/database/dbConfig.js
@@ -1,6 +1,6 @@
 export default {
   host: 'ec2-107-22-238-217.compute-1.amazonaws.com',
-  user: 'slgwzksqbpgwpf',
+  user: 'iulvfbrxbpshrt',
   database: 'd8q2lbmes6bdmu',
   password: 'af8c9f7c7fba21911a68ef8741adb4690d82f6f0f88992ffb9b067ebe4442f8c',
   port: 5432,
